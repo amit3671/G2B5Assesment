45cf957a7e41c84e5419557e16d4ea3bc46c962d
amit3671 <127406102+amit3671@users.noreply.github.com>
2023-06-19 21:46:57 +0530

    Update SuperDepartment.java

--- SuperDepartment.java
+++ SuperDepartment.java
@@ -20,35 +20,5 @@ public class SuperDepartment {
   {
 	   return "Today is not a Holiday \n";
   }
-
-	
-public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-	SuperDepartment obj1 = new AdminDepartment();
-    SuperDepartment obj2 = new HrDepartment();
-	SuperDepartment obj3 =new TechDepartment();
-	HrDepartment Hr = new HrDepartment();
-	TechDepartment tech = new TechDepartment();
-	
-	System.out.println(obj1.departmentName());
-	System.out.println(obj1.getTodaysWork());
-	System.out.println(obj1.getWorkDeadline());
-	System.out.println(obj1.isTodayAHoliday());
-	
-	System.out.println(obj2.departmentName());
-	System.out.println(Hr.doActivity());
-	System.out.println(obj2.getTodaysWork());
-	System.out.println(obj2.getWorkDeadline());
-	System.out.println(obj2.isTodayAHoliday());
-	
-	System.out.println(obj3.departmentName());
-	System.out.println(obj3.getTodaysWork());
-	System.out.println(obj3.getWorkDeadline());
-	System.out.println(tech.getTechStackInformation());
-	System.out.println(obj3.isTodayAHoliday());
-	
-
-	}
-
 }
+	
